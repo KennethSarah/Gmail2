6562dd9061db6badf2c8e99d735f4292c25791de
Kenneth Sarah <kennethnifemi@gmail.com>
2023-11-30 21:01:27 +0100

    maill

--- script.js
+++ script.js
@@ -6,131 +6,131 @@ const data = [
     sender: "Unknown",
     date: "20:15",
     message:
-      "NOT ADMITTED ON THE MERIT LIST? HERE ARE REASONS WHY - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
   },
   // 2
   {
     sender: "Twitter",
     date: "07:13",
     message:
-      "NOT ADMITTED ON THE MERIT LIST? HERE ARE REASONS WHY - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
   },
   //  3
   {
     sender: "The Punch",
     date: "05:01",
     message:
-      "NOT ADMITTED ON THE MERIT LIST? HERE ARE REASONS WHY - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
   },
   // 4
   {
     sender: "Instagram",
     date: "Jan 11",
     message:
-      "NOT ADMITTED ON THE MERIT LIST? HERE ARE REASONS WHY - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
   },
   // 5
   {
     sender: "Qatar",
     date: "Nov 24",
     message:
-      "NOT ADMITTED ON THE MERIT LIST? HERE ARE REASONS WHY - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
   },
   // 6
   {
     sender: "John Mark",
     date: "Oct 26",
     message:
-      "NOT ADMITTED ON THE MERIT LIST? HERE ARE REASONS WHY - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
   },
   // 7
   {
     sender: "Whatsapp",
     date: "Dec 21",
     message:
-      "NOT ADMITTED ON THE MERIT LIST? HERE ARE REASONS WHY - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
   },
   // 8
   {
     sender: "Google",
     date: "Dec 10",
     message:
-      "NOT ADMITTED ON THE MERIT LIST? HERE ARE REASONS WHY - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
   },
   // 9
   {
     sender: "Twitter",
     date: "May 20",
     message:
-      "NOT ADMITTED ON THE MERIT LIST? HERE ARE REASONS WHY - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
   },
   // 10
   {
     sender: "Facebook",
     date: "Feb 23",
     message:
-      "NOT ADMITTED ON THE MERIT LIST? HERE ARE REASONS WHY - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
   },
   // 11
   {
     sender: "Tiktok",
     date: "Jun 25",
     message:
-      "NOT ADMITTED ON THE MERIT LIST? HERE ARE REASONS WHY - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
   },
   // 12
   {
     sender: "Whatsapp",
     date: "Sep 28",
     message:
-      "NOT ADMITTED ON THE MERIT LIST? HERE ARE REASONS WHY - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
   },
   // 13
   {
     sender: "Upwork",
     date: "Jan 17",
     message:
-      "NOT ADMITTED ON THE MERIT LIST? HERE ARE REASONS WHY - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
   },
   // 14
   {
     sender: "LinkedIn",
     date: "Nov 22",
     message:
-      "NOT ADMITTED ON THE MERIT LIST? HERE ARE REASONS WHY - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
   },
   // 15
   {
     sender: "Facebook",
     date: "Apr 23",
     message:
-      "NOT ADMITTED ON THE MERIT LIST? HERE ARE REASONS WHY - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
   },
   // 16
   {
     sender: "Instagram",
     date: "Jul 23",
     message:
-      "NOT ADMITTED ON THE MERIT LIST? HERE ARE REASONS WHY - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
   },
   // 17
   {
     sender: "Twitter",
     date: "Dec 31",
     message:
-      "NOT ADMITTED ON THE MERIT LIST? HERE ARE REASONS WHY - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
   },
   {
     sender: "Twitter",
     date: "Dec 31",
     message:
-      "NOT ADMITTED ON THE MERIT LIST? HERE ARE REASONS WHY - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
   },
   {
     sender: "Twitter",
     date: "Dec 31",
     message:
-      "NOT ADMITTED ON THE MERIT LIST? HERE ARE REASONS WHY - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
   },
 ];
 
